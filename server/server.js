9826ec82b91262b3fb57409538c11b59e72eecae
Vojislav Stanojevikj <vojislav.stanojevikj.mk@gmail.com>
2019-01-20 18:24:48 +0100

    users collection added.

--- server/server.js
+++ server/server.js
@@ -5,15 +5,30 @@ mongoose.connect('mongodb://172.17.0.2:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
+
+const User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1
+    }
+});
+
 //
 // const toDoActivity = new Todo({text: 'Cock Dinner'});
 //
@@ -23,12 +38,30 @@ const Todo = mongoose.model('Todo', {
 //     console.error('Saving Todo failed!', error);
 // });
 
-const newTodo = new Todo({text: 'Go to the gym', completed: true, completedAt: new Date().getTime()});
+// const newTodo = new Todo({text: '   Edit this video'});
+//
+// newTodo.save()
+//     .then(document => {
+//         console.log('Saved Todo!', document);
+//     }).catch(error => {
+//         console.error('Saving Todo failed!', error);
+//     });
 
-newTodo.save()
+
+const firstUser = new User({email: 'Test@test.com'});
+
+firstUser.save()
     .then(document => {
-        console.log('Saved Todo!', document);
+        console.log('User saved!', JSON.stringify(document, undefined, 2));
     }).catch(error => {
-        console.error('Saving Todo failed!', error);
+        console.error('Error saving user!', error);
     });
 
+// const firstUser = new User({email: {}});
+//
+// firstUser.save()
+//     .then(document => {
+//         console.log('User saved!', JSON.stringify(document, undefined, 2));
+//     }).catch(error => {
+//     console.error('Error saving user!', error);
+// });
